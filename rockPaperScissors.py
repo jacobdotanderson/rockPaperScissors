754706b7523efb1b7afb27d50b6f2e923238ae93
jacobdotanderson <68460116+jacobdotanderson@users.noreply.github.com>
2020-07-18 02:43:44 -0500

    Update rockPaperScissors.py
    Added .lower() to inputs to allow for inputs to not require capitalization and removed "Check your spelling!" from else statement.

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -7,22 +7,22 @@ while player==False:
     player=input("Rock, Paper, Scissors?")
     if player==computer:
         print("Tie!")
-    elif player=="Rock":
+    elif player=="Rock".lower():
         if computer=="Paper":
             print("You lose!", computer, "covers", player)
         else:
             print("You win!", player, "smashes", computer)
-    elif player=="Paper":
+    elif player=="Paper".lower():
         if computer=="Scissors":
             print("You lose!", computer,"cut", player)
         else:
             print("You win!", player,"covers", computer)
-    elif player=="Scissors":
+    elif player=="Scissors".lower():
         if computer=="Rock":
             print("You lose!", computer, "smashes", player)
         else:
             print("You win!", player, "cut", computer)
     else:
-        print("That's not a valid play.  Check your spelling!")
+        print("That's not a valid play.")
     player=False
     computer=t[randint(0,2)]
